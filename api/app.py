1ae4f3ca9ffa627936e330c6b0a59bf8a09399c5
yuta-tsurusaki <y.tsurusaki@prog-8.com>
2020-10-31 17:17:37 +0900

    Create api

---
+++ api/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+app = Flask(__name__)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+def parse(text):
+	textList = list(text)
+	outputText = ""
+	for c in textList:
+		outputText += c + "w"
+	return outputText	
+
+@app.route('/')
+def health():
+    return 'OK'
+
+@app.route('/api/kusa', methods=['POST'])
+def parseText():
+	inputText = request.json['text']
+	outputText = parse(inputText)
+	return jsonify({"kusaText": outputText})
