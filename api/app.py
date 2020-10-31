706649516c79f720bb50cf88926915c7ddd5044b
yuta-tsurusaki <y.tsurusaki@prog-8.com>
2020-10-31 19:08:29 +0900

    Remove cors specifications

--- api/app.py
+++ api/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+cors = CORS(app)
 
 def parse(text):
 	textList = list(text)
